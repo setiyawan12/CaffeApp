619b453c951c197b529eab11f886d75ae07a7c59
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-08-20 03:39:08 +0700

    3. OnFailure

--- app/src/main/java/yayang/setiyawan/caffe/Fragment/HomeFragment.kt
+++ app/src/main/java/yayang/setiyawan/caffe/Fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_home.view.*
@@ -45,7 +46,7 @@ class HomeFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<ResponModel>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(requireActivity(), "Tidbisa koneksi ke server", Toast.LENGTH_LONG).show()
             }
 
         })
